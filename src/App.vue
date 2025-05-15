42eb0a52904c3ebf388c12a22378bbd769535de7
Ingryd <ingrydcduarte@gmail.com>
2025-05-15 10:39:12 -0300

    setup: workspace project (vue + vite)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HomeView from './page/Home.vue'
+</script>
+
+<template>
+  <div>
+    <HomeView />
+    </div>
+</template>
+
+<style scoped>
+
+.button:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+</style>
---
+++ src/page/Home.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="centralized-title">
+        <h2 class="home-title">A progressive folder organizer</h2>
+        <p>Lorem ipsum</p>
+        <router-link to="/about">
+            <button class="btn-primary">Ir para página de instalação</button>
+        </router-link>
+    </div>
+</template>
+
+<style scoped>
+.centralized-title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.home-title {
+    font-size: 3rem;
+    color: #646cffaa;
+    text-align: center;
+    animation: lights 5s 750ms linear infinite;
+}
+
+@keyframes lights {
+    0% {
+        color: hsl(230, 40%, 80%);
+        text-shadow:
+            0 0 1em hsla(320, 100%, 50%, 0.2),
+            0 0 0.125em hsla(320, 100%, 60%, 0.3),
+            -1em -0.125em 0.5em hsla(40, 100%, 60%, 0),
+            1em 0.125em 0.5em hsla(200, 100%, 60%, 0);
+    }
+
+    30% {
+        color: hsl(230, 80%, 90%);
+        text-shadow:
+            0 0 1em hsla(320, 100%, 50%, 0.5),
+            0 0 0.125em hsla(320, 100%, 60%, 0.5),
+            -0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2),
+            0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4);
+    }
+
+    40% {
+        color: hsl(230, 100%, 95%);
+        text-shadow:
+            0 0 1em hsla(320, 100%, 50%, 0.5),
+            0 0 0.125em hsla(320, 100%, 90%, 0.5),
+            -0.25em -0.125em 0.125em hsla(40, 100%, 60%, 0.2),
+            0.25em 0.125em 0.125em hsla(200, 100%, 60%, 0.4);
+    }
+
+    70% {
+        color: hsl(230, 80%, 90%);
+        text-shadow:
+            0 0 1em hsla(320, 100%, 50%, 0.5),
+            0 0 0.125em hsla(320, 100%, 60%, 0.5),
+            0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2),
+            -0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4);
+    }
+
+    100% {
+        color: hsl(230, 40%, 80%);
+        text-shadow:
+            0 0 1em hsla(320, 100%, 50%, 0.2),
+            0 0 0.125em hsla(320, 100%, 60%, 0.3),
+            1em -0.125em 0.5em hsla(40, 100%, 60%, 0),
+            -1em 0.125em 0.5em hsla(200, 100%, 60%, 0);
+    }
+
+}
+</style>
